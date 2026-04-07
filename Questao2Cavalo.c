e8d187b9299fee4d8ac847559c42011fed55ccff
Cauê (vvankxs7) <cauecpereira06@gmail.com>
2026-04-07 14:31:26 -0300

    Add program to calculate horseshoe requirements

---
+++ Questao2Cavalo.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int cavl,ferra;
+    printf("Quant de Cavalos");
+    scanf("%d",&cavl);
+    ferra = cavl*4;
+    printf("Sao necessarios %d ferraduras",ferra);
+    
+    return 0;
+}
